d5a476c1d123a97b22bfa504c2c89529abdfa36e
Benjamin Eunice <beunic1@lsu.edu>
2022-03-30 20:42:12 -0500

    Initial commit.

---
+++ addition.py
@@ -0,0 +1,14 @@
+import math
+
+print("Homework doer 3000 by some white mayo dumbass in Louisiana.")
+
+numone = input("Enter a fucking number:")
+
+numtwo = input("Enter a second bitchass number:")
+
+output = float(numone) + float(numtwo)
+
+if output.is_integer() == True:
+    print(int(output))
+else:
+    print(output)
